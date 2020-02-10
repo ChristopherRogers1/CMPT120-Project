7f4d07d88429b24a42dacb1a48c86adb2ad1a626
Christopher Rogers <Christopher.Rogers1@marist.edu>
2020-02-09 21:01:47 -0500

    Added the first scenario the player encounters, as well as a money function and variable.

--- tale.py
+++ tale.py
@@ -1,3 +1,6 @@
+def getPaid(wallet):
+    return 10 + wallet
+
 def main():
 
     print()
@@ -19,6 +22,24 @@ def main():
     job = input("")
 
     print("Ah yes, " + name + ", the great " + job + " of " + home + ".")
+
+    wallet = 0
+
+    print("It's getting late, and you've got a long day ahead of you; why not take a nap?")
+
+    input("Press enter to go to sleep...")
+
+    print("...")
+
+    print("You awake to a fire! Your home is burning down! In a panic, you run to the exit, leaving behind all of your money and belongings. The town is being pillaged by the badguyians to the north! You run to the east, towards the woods. Once you arrive, you find a crisp ten dollar coin. Sweet!")
+
+    wallet = getPaid(wallet)
+
+    print("You now have " + str(wallet) + " dollars.")
+
+    input("Press enter to travel into the forest...")
+
+    print("You travel deeper into the forest, leaving your burning town behind.
 main()
 
 
