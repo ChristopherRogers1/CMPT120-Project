e9472b7a787779f0ff9175b109fb8d3a1ddd091f
Christopher Rogers <Christopher.Rogers1@marist.edu>
2020-02-09 20:35:54 -0500

    Added the intro and character creation.

---
+++ tale.py
@@ -0,0 +1,24 @@
+def main():
+
+    print()
+    print("---------------------------")
+    print("Welcome to THE HEROIC TALE!")
+    print("---------------------------")
+    print()
+
+    print("Let's start with some information about yourself, traveler. What is your name?")
+
+    name = input("")
+    
+    print("Welcome to Eawrsa, " + name + ". Remind me, where do you come from?")
+
+    home = input("")
+
+    print("Interesting. Only one more question, what was your profession in your homeland?")
+
+    job = input("")
+
+    print("Ah yes, " + name + ", the great " + job + " of " + home + ".")
+main()
+
+
